6b3a31a0d0cc103c8236091a3ebaa7cec4287a98
Abdool Shakur <ass200501@darden.com>
2019-05-30 10:33:31 -0400

    allocate and initailize variables

--- Assignment_HW1.c
+++ Assignment_HW1.c
@@ -1,28 +1,55 @@
+/* libararies */
 #include <stdio.h>
 #include <stdlib.h>
 
+/* definitions */
 #define MAX_STACK_HEIGHT 23
 #define MAX_CODE_LENGTH  500
 #define MAX_LEXI_LEVELS  3
 
-int PC   = 0;
-int * BP = NULL;
-int * GP = NULL;
-int * SP = NULL;
 
+/* registers & stack */
+int  * RF;                  // register file (0-7) 
+int  * PC;                  // program counter
+int  * BP;                  // base pointer
+int  * GP;                  // global pointer
+int  * SP;                  // stack pointer
+int *  Stack;               // stack (0-22)
+
+/* instruction register */
 typedef struct IR
 {
-    int op;
-    int r;
-    int l;
-    int m;
+    int op;                  // operation code
+    int r;                   // register 
+    int l;                   // lexicographical level
+    int m;                   // modifier
 } IR;
 
+/* cycles */
 void fetch();
 void execute(IR ir);
 
-void main()
+/* main driver */
+int main()
 {
 
-    return 0;
+    // allocate variables
+    Stack = calloc(sizeof(int), 23);
+    RF    = calloc(sizeof(int), 8);
+    PC    = calloc(sizeof(int), 1);
+    BP    = calloc(sizeof(int), 1);
+    GP    = calloc(sizeof(int), 1);
+    SP    = calloc(sizeof(int), 1);
+   
+    // initialize variables
+    PC =  0;
+    BP =  0;
+    GP = -1;
+    SP = MAX_STACK_HEIGHT;
+
+    // allocate structs
+    IR * IR = calloc(sizeof(IR), 1);
+
+
+    return EXIT_SUCCESS;
 }
