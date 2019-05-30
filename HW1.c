cf4c631e4e51c104ccd1bda69129ecee4c2c691d
Abdool Shakur <ass200501@darden.com>
2019-05-30 12:00:16 -0400

    added the cycles and testing, working on switches

--- HW1.c
+++ HW1.c
@@ -15,7 +15,7 @@ int  * PC;                  // program counter
 int  * BP;                  // base pointer
 int  * GP;                  // global pointer
 int  * SP;                  // stack pointer
-int *  Stack;               // stack (0-22)
+int *  DS;                  // datastack (0-22)
 
 
 /* instruction register */
@@ -29,16 +29,19 @@ typedef struct IR
 
 
 /* cycles */
-void fetch();
-void execute(IR ir);
+void fetch(IR * IR);
+void execute(IR * IR);
 
+/* helpers */
+int  base(int l, int base);
+void print();
 
 /* main driver */
 int main()
 {
 
     // allocate variables
-    Stack = calloc(sizeof(int), 23);
+    DS    = calloc(sizeof(int), 23);
     RF    = calloc(sizeof(int), 8);
     PC    = calloc(sizeof(int), 1);
     BP    = calloc(sizeof(int), 1);
@@ -57,7 +60,7 @@ int main()
     // testing
     for(int i = 0; i < 23; i++)
     {
-        printf("stack[%d] = %d\n", i, Stack[i]);
+        printf("stack[%d] = %d\n", i, DS[i]);
     }
      for(int i = 0; i < 8; i++)
     {
@@ -67,8 +70,91 @@ int main()
     printf("bp = %d\n", *BP);
     printf("gp = %d\n", *GP);
     printf("sp = %d\n", *SP);
+    
 
-    //
+    // fetch instruction
+    fetch(IR);
+
+    // execute instruction
+    execute(IR);
 
     return EXIT_SUCCESS;
+}
+
+
+void fetch(IR * IR)
+{
+    scanf("%d", &IR->op);
+    scanf("%d", &IR->r);
+    scanf("%d", &IR->l);
+    scanf("%d", &IR->m);
+    return;
+}
+
+
+void execute(IR * IR)
+{
+
+    // testing
+    printf("        \n");
+    printf("op - %d \n", IR->op);
+    printf("r  - %d \n", IR->r);
+    printf("l  - %d \n", IR->l);
+    printf("m  - %d \n", IR->m);
+
+    switch(IR->op)
+    {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        case 7:
+            break;
+        case 8:
+            break;
+        case 9:
+            break;
+        case 10:
+            break;
+        case 11:
+            break;
+        case 12:
+            break;
+        case 13:
+            break;
+        case 14:
+            break;
+        case 15:
+            break;
+        case 16:
+            break;
+        case 17:
+            break;
+        case 18:
+            break;
+        case 19:
+            break;
+        case 20:
+            break;
+        case 21:
+            break;
+        case 22:
+            break;
+        case 23:
+            break;
+        case 24:
+            break;
+        default:
+            break;
+    }
+
+    return;
 }
